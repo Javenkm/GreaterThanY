1c072de8207dee4c4afb8cd0b30baa364d66086a
Javenkm <Javenkm@gmail.com>
2021-05-26 01:42:20 -0700

    init commit

---
+++ GreaterThanY.js
@@ -0,0 +1,17 @@
+var Y = 24
+function greaterThanY(arr) {
+    for(var i = 0; i > Y; i++) {
+        return arr > Y
+        }
+    }
+console.log(greaterThanY([3, 76, 5, 28, 4, 91, 23, 25, 69, 0]));
+
+
+// function findGreater(arr, element) {
+
+//     return arr.filter((item) => {
+//         return item > element
+//     })
+// }
+
+// console.log(findGreater([10, 25, 16, -5, 30, 15, 24], 16));
